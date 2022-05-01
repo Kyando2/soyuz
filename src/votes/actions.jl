9a4e56c2da1302a1bd1e20f45389ad09fe66699d
キャンド <amehikoji@gmail.com>
2022-05-01 16:13:09 -0400

    add categories as well

--- src/votes/actions.jl
+++ src/votes/actions.jl
@@ -13,10 +13,11 @@ struct VoteAction
 end
 CreateTextChannelVoteAction(name::String, topic::String, position::Int, parent::Ekztazy.Snowflake, nsfw::Bool) = VoteAction(va_create_channel, name, 0, topic, position, parent, nsfw)
 CreateVoiceChannelVoteAction(name::String, position::Int, parent::Ekztazy.Snowflake) = VoteAction(va_create_channel, name, 2, missing, position, parent, missing)
+CreateCategoryChannelVoteAction(name::String, position::Int) = VoteAction(va_create_channel, name, 4, missing, position, missing, missing)
 
 function perform(a::VoteAction)
     if a.type == va_create_channel
-        if a.parent != 0 
+        if !(ismissing(a)) && a.parent != 0 
             Ekztazy.create_guild_channel(c, parse(Int, g), name=a.name, type=a.channel_type, topic=a.topic, position=a.position, parent_id=a.parent, nsfw=a.nsfw)
         else 
             Ekztazy.create_guild_channel(c, parse(Int, g), name=a.name, type=a.channel_type, topic=a.topic, position=a.position, nsfw=a.nsfw)
@@ -26,6 +27,13 @@ end
 
 function msg(a::VoteAction)
     if a.type == va_create_channel
-        return "Proposal to create channel called $(a.name)."
+        type = if a.channel_type == 0 
+            "text"
+         elseif a.channel_type == 2
+             "voice" 
+        elseif a.channel_type == 4 
+            "category"
+        end
+        return "Proposal to create $(type) channel called $(a.name)."
     end
 end
--- src/votes/channel.jl
+++ src/votes/channel.jl
@@ -2,20 +2,29 @@ command!(c, g, "create_text_channel", "propose to create a text channel", legacy
     [String, "name", "the name of the channel"],
     [String, "topic", "the topic of the channel"],
     [Int, "position", "the sorting position of the channel"],
-    [Int, "parent", "the category for this channel, 0 if none"],
+    [String, "parent", "the category for this channel, 0 if none"],
     [Bool, "nsfw", "whether the channel is nsfw"]
-)) do ctx, name::String, topic::String, position::Int, parent::Int, nsfw::Bool
+)) do ctx, name::String, topic::String, position::Int, parent::String, nsfw::Bool
     global c, votemap
-    create_vote(CreateTextChannelVoteAction(name, topic, position, Ekztazy.Snowflake(parent), nsfw))
+    create_vote(CreateTextChannelVoteAction(name, topic, position, parse(Ekztazy.Snowflake, parent), nsfw))
     Ekztazy.reply(c, ctx, content="Succesfully created vote proposal.")
 end
 
 command!(c, g, "create_voice_channel", "propose to create a voice channel", legacy=false, options=Options(
     [String, "name", "the name of the channel"],
     [Int, "position", "the sorting position of the channel"],
-    [Int, "parent", "the category for this channel, 0 if none"],
-)) do ctx, name::String, position::Int, parent::Int
+    [String, "parent", "the category for this channel, 0 if none"],
+)) do ctx, name::String, position::Int, parent::String
     global c, votemap
-    create_vote(CreateVoiceChannelVoteAction(name, position, Ekztazy.Snowflake(parent)))
+    create_vote(CreateVoiceChannelVoteAction(name, position, parse(Ekztazy.Snowflake, parent)))
+    Ekztazy.reply(c, ctx, content="Succesfully created vote proposal.")
+end
+
+command!(c, g, "create_category", "propose to create a category", legacy=false, options=Options(
+    [String, "name", "the name of the channel"],
+    [Int, "position", "the sorting position of the channel"],
+)) do ctx, name::String, position::Int
+    global c, votemap
+    create_vote(CreateCategoryChannelVoteAction(name, position))
     Ekztazy.reply(c, ctx, content="Succesfully created vote proposal.")
 end
