15e3eaddd21f4b48e611c755bb6be9b418dd0b50
キャンド <amehikoji@gmail.com>
2022-06-30 21:02:52 -0400

    still

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from discord.ext import commands
+from typing import Optional
 
+from discord.ext import commands
+from discord import app_commands
 from lib.misc import token
 from lib.permanent import PermanentJsonContext
+from lib.voting.system import vote_factory, action_factory
 
 import discord
 
-pj = PermanentJsonContext("test")
+MY_GUILD = discord.Object(PermanentJsonContext("consts").guild)
 
 class Bot(commands.Bot):
     def __init__(self):
@@ -17,6 +20,23 @@ class Bot(commands.Bot):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
 
+    async def setup_hook(self):
+        pj = PermanentJsonContext("votes")
+        for k in pj.structure.keys():
+            c = pj[k]
+            action = action_factory(c["action"], c["action_d"])
+            self.add_view(vote_factory(action, c["count"], c["threshold"], c["message_id"]))
+        self.tree.copy_global_to(guild=MY_GUILD)
+        await self.tree.sync(guild=MY_GUILD)
+
 bot = Bot()
+
+@bot.tree.command()
+@app_commands.describe(
+    name="The name of the channel you want to create",
+    category="The category you want to add the channel to",
+)
+async def create_channel(interaction: discord.Interaction, name: str, category: Optional[discord.CategoryChannel]):
+    await interaction.response.send_message(f'Hi, {interaction.user.mention}')
 
 bot.run(token())
