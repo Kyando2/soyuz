46d9c055a08f691b6ca4cc3650f900388941f7cd
キャンド <amehikoji@gmail.com>
2022-07-27 11:04:52 -0400

    add delete message action

--- lib/voting/actions/message_actions.py
+++ lib/voting/actions/message_actions.py
@@ -19,6 +19,21 @@ class SendMessageAction(Action):
     def message(self):
         return f'Proposal to send the message `"{ self.text }"` to channel the <#{ self.channel_id }>.'
 
+class DeleteMessageAction(Action):
+    ID = 1
+
+    def __init__(self, channel_id, message_id):
+        super().__init__(channel_id=channel_id, message_id=message_id)
+    
+    async def run(self, bot: commands.Bot):
+        gu = guild(bot)
+        ct = await gu.fetch_channel(self.channel_id)
+        mg = await ct.fetch_message(self.message_id)
+        await mg.delete()
+    
+    def message(self):
+        return f'Proposal to delete a message in <#{ self.channel_id }>\n[Message link](https://discord.com/channels/969011076144431165/{self.channel_id}/{self.message_id})'
+
 def register_message_actions(bot: commands.Bot, f):
     message_g = app_commands.Group(name="message", description="actions related to messages")
 
@@ -29,6 +44,14 @@ def register_message_actions(bot: commands.Bot, f):
             await interaction.response.send_message(f'You cannot send messages to <#{ channel.id }>.', ephemeral=True)
         else:
             await f(bot, interaction, 2, channel_id=channel.id, text=text)
+    
+    @message_g.command(name="nonthread")
+    @app_commands.describe(channel="The channel in which the message is", message_id="The message to delete")
+    async def delete_channel(interaction: discord.Interaction, channel: discord.abc.GuildChannel, message_id: str):
+        if channel.id in CONSTS.notouch:
+            await interaction.response.send_message(f'You cannot delete a message in <#{ channel.id }>.', ephemeral=True)
+        else:
+            await f(bot, interaction, 1, channel_id=channel.id, message_id=message_id)
 
 
     bot.tree.add_command(message_g)
