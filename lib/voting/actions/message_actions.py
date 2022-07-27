0856c2da1960c6e0aec8a0e2c9d358786df05a8b
キャンド <amehikoji@gmail.com>
2022-07-27 11:23:25 -0400

    fix

--- lib/voting/actions/message_actions.py
+++ lib/voting/actions/message_actions.py
@@ -32,7 +32,7 @@ class DeleteMessageAction(Action):
         await mg.delete()
     
     def message(self):
-        return f'Proposal to delete a message in <#{ self.channel_id }>\n[Message link](https://discord.com/channels/969011076144431165/{self.channel_id}/{self.message_id})'
+        return f'Proposal to delete a message in <#{ self.channel_id }>\n[Message](https://discord.com/channels/969011076144431165/{self.channel_id}/{self.message_id})'
 
 def register_message_actions(bot: commands.Bot, f):
     message_g = app_commands.Group(name="message", description="actions related to messages")
@@ -47,7 +47,7 @@ def register_message_actions(bot: commands.Bot, f):
     
     @message_g.command(name="delete")
     @app_commands.describe(channel="The channel in which the message is", message_id="The message to delete")
-    async def delete_message(interaction: discord.Interaction, channel: discord.abc.GuildChannel, message_id: str):
+    async def delete_message(interaction: discord.Interaction, channel: discord.TextChannel, message_id: str):
         if channel.id in CONSTS.notouch:
             await interaction.response.send_message(f'You cannot delete a message in <#{ channel.id }>.', ephemeral=True)
         else:
