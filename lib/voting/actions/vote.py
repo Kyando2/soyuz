a2355d188a031c9b762e56eb41095cc99dd744eb
キャンド <amehikoji@gmail.com>
2022-07-28 09:28:50 -0400

    register more actions

--- lib/voting/actions/vote.py
+++ lib/voting/actions/vote.py
@@ -1,7 +1,10 @@
 from discord.ext import commands
 from lib.voting.actions.channel_actions import register_channel_actions
+from lib.voting.actions.emoji_actions import register_emoji_actions
 from lib.voting.actions.message_actions import register_message_actions
+from lib.voting.actions.permission_actions import register_permission_actions
 from lib.voting.actions.role_actions import register_role_actions
+from lib.voting.actions.setting_actions import register_settings_actions
 from lib.voting.actions.user_actions import register_user_actions
 
 def register_actions(bot: commands.Bot, f):
@@ -9,3 +12,6 @@ def register_actions(bot: commands.Bot, f):
     register_message_actions(bot, f)
     register_role_actions(bot, f)
     register_user_actions(bot, f)
+    register_permission_actions(bot, f)
+    register_settings_actions(bot, f)
+    register_emoji_actions(bot, f)
